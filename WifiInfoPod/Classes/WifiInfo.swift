edb5294c9f9402f6a488ffc50fe74f233c36f30f
Matt Gross <greezy.gross@gmail.com>
2019-05-28 03:06:13 -0400

    update

--- WifiInfoPod/Classes/WifiInfo.swift
+++ WifiInfoPod/Classes/WifiInfo.swift
@@ -7,11 +7,21 @@ public class WifiInfo: NSObject {
         var resp: [String: Any] = ["connected": false]
         var ssid: String?
         var bssid: String?
+        print("Calling CNCopySupportedInterfaces()")
         if let interfaces = CNCopySupportedInterfaces() as NSArray? {
+            print("Type cast to NSArray SUCCESS")
             for interface in interfaces {
+                print("new interface")
+                print(interface)
                 if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as! CFString) as NSDictionary? {
+                    print("new interfaceInfo")
+                    print(interface)
                     ssid = interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
+                    print("ssid")
+                    print(ssid)
                     bssid = interfaceInfo[kCNNetworkInfoKeyBSSID as String] as? String
+                    print("bssid")
+                    print(bssid)
                     break
                 }
             }
